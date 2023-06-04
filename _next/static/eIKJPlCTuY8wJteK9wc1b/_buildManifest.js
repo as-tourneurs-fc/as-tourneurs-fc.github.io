7512665b91b05cb1f7ea3ef838bb2d020d69db10
gsmartagence <gilles@smartagence.com>
2023-06-04 14:37:28 +0200

    nojekill

---
+++ _next/static/eIKJPlCTuY8wJteK9wc1b/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/884-18baca13a635452d.js","static/css/53fa15469dc309b1.css","static/chunks/pages/index-7b34bf0c8524be60.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
