b2f41d94192e4863a4dfc8f37f03ce11dbd61278
gsmartagence <gilles@smartagence.com>
2023-06-06 08:58:32 +0200

    covers added

--- _next/static/QJUi5KwZDd9Sd17_AQNTH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/884-18baca13a635452d.js","static/css/53fa15469dc309b1.css","static/chunks/pages/index-a6e3cf0677e839ba.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
