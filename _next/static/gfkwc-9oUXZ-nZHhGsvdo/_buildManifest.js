3739be83f4dfe64e316b10586098cc2ab8f5ab0c
gsmartagence <gilles@smartagence.com>
2023-06-06 10:25:25 +0200

    re7 slider

---
+++ _next/static/gfkwc-9oUXZ-nZHhGsvdo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/463-642001be4358537b.js","static/css/53fa15469dc309b1.css","static/chunks/pages/index-f4e0a14d1ca6ac3e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
